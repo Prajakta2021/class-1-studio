bfc058487eb3232f26fa181bb65fd49a1381aca4
Prajakta <Prajakta0709@gmail.com>
2021-07-16 16:03:09 -0500

    Add project files.

---
+++ 26 Studio Area of a Circle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _26_Studio_Area_of_a_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // double pi = Math.PI;
+            Console.WriteLine("Please enter a radius of circle : ");
+            double radius = double.Parse(Console.ReadLine());
+            //radius should be "double" data type to save int as well as decimal numbers.
+            double areaOfCircle = Math.PI * Math.Pow(radius, 2);
+            double circumferenceOfCircle = 2 * Math.PI * radius;
+            double diameterOfCircle = 2 * radius;
+            Console.WriteLine("Area of circle is " + areaOfCircle + " square units.");
+            Console.WriteLine("Circumference of circle is " + circumferenceOfCircle + " units.");
+            Console.WriteLine("Diameter of circle is " + diameterOfCircle + " units.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Road trip!/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Road_trip_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a radius of circle  : ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a miles per gallon of the car : ");
+            double mpg = double.Parse(Console.ReadLine());
+            double aroundTheCircle = 2 * Math.PI * radius;                       
+            Console.WriteLine("you need " + aroundTheCircle/mpg + " gallons of gas to go around the circle of radius " +radius+ " miles.");
+            Console.ReadLine();
+        }
+    }
+}
